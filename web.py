aed6e9a0c9438d736e1fbb6e31216b79da572fef
张睿 <120297419+WatchMan123@users.noreply.github.com>
2023-12-30 17:19:24 +0800

    Update web.py

--- web.py
+++ web.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 def loadpdf():
     st.title("欧洲自行车骑行宣言")
     st.write("")
-    pdf_file = "自行车骑行宣言Latex.pdf"
+    pdf_file = "自行车骑行宣言tex.pdf"
     with open(pdf_file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf">'
