ea46dbb4bfb383760b0fa93e3110e608e8ac2be3
张睿 <120297419+WatchMan123@users.noreply.github.com>
2023-12-30 18:44:54 +0800

    Update web.py

--- web.py
+++ web.py
@@ -8,8 +8,6 @@ def loadpdf():
     st.title("欧洲自行车骑行宣言")
     st.write("")
     pdf_file = "Latex.pdf"
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
     with open(pdf_file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     # pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf"></iframe>'
