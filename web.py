a48134fb8d8937d4bb9693ae5dcd08f2638d51a2
张睿 <120297419+WatchMan123@users.noreply.github.com>
2023-12-30 18:52:48 +0800

    Update web.py

--- web.py
+++ web.py
@@ -11,8 +11,6 @@ def loadpdf():
     pdf_file = "Latex.pdf"
     with open(pdf_file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    # pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf"></iframe>'
-    # st.markdown(pdf_display, unsafe_allow_html=True)
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
     
